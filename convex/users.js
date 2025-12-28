5022945269d9ac1eb444eeccc3043977d90f1170
Aly Mama <52185940+alywheng27@users.noreply.github.com>
2025-12-28 14:57:19 +0800

    Working with the 'Purchase Button' in course page

--- convex/users.js
+++ convex/users.js
@@ -1,5 +1,5 @@
-import { mutation } from "./_generated/server";
-import { v } from "convex/values";
+import { mutation, query} from "./_generated/server";
+import { ConvexError, v } from "convex/values";
 
 export const createUser = mutation({
   args: {
@@ -24,5 +24,55 @@ export const createUser = mutation({
     })
 
     return userId
+  }
+})
+
+export const getUserByClerkId = query({
+  args: {  clerkId: v.string() },
+  handler: async (ctx, args) => {
+    return await ctx.db.query("users")
+    .withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
+    .unique()
+  }
+})
+
+export const getUserByStripeCustomerId = query({
+  args: { stripeCustomerId: v.string() },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("users")
+      .withIndex("by_stripeCustomerId", (q) => q.eq("stripeCustomerId", args.stripeCustomerId))
+      .unique()
+  }
+})
+
+export const getUserAccess = query({
+  args: { userId: v.id("users"), courseId: v.id("courses") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity()
+
+    if(!identity) {
+      throw new ConvexError("Unauthirized")
+    }
+
+    const user = await ctx.db.get(args.userId)
+    if(!user) {
+      throw new ConvexError("User not found")
+    }
+
+    if(user.currentSubscriptionId) {
+      const subscription = await ctx.db.get(user.currentSubscriptionId)
+
+      if(subscription && subscription.status === "active") {
+        return { hasAccess: true, accessType: "subscription" }
+      }
+    }
+
+    //check for individual course access
+    const purchase = await ctx.db.query("purchases")
+      .withIndex("by_userID_and_courseId", (q) => q.eq("userId", args.userId).eq("courseId", args.courseId))
+      .unique()
+
+      return { hasAccess: false }
   }
 })
